99b7898718538bead69e2f64383c9bd5dadc4273
Rubén Astráin <ruben.astrain@bravent.net>
2022-03-07 08:48:19 +0100

    Value object del lugar de la carrera

---
+++ src/Domain/Races/Exceptions/RaceLocationException.cs
@@ -0,0 +1,10 @@
+namespace TrailRunning.Races.Management.Domain.Races.Exceptions;
+
+public class RaceLocationException : Exception
+{
+    public RaceLocationException(string message)
+        : base(message)
+    {
+
+    }
+}
--- src/DomainTests/Races/RacesTests.cs
+++ src/DomainTests/Races/RacesTests.cs
@@ -9,9 +9,20 @@ public class races_should
     public void not_allow_create_if_date_is_past()
     {
         var date = DateTime.Now.AddDays(-1);
+        var hour = new TimeOnly(9, 0);
 
-        var act = () => RaceDate.Create(DateOnly.FromDateTime(date));
+        var act = () => RaceDate.Create(DateOnly.FromDateTime(date), hour);
 
         act.Should().Throw<RaceDatePastException>();
+    }
+
+    [Theory]
+    [InlineData("")]
+    [InlineData(" ")]
+    public void not_allow_create_if_location_is_empty(string location)
+    {
+        var act = () => RaceLocation.Create(location);
+
+        act.Should().Throw<RaceLocationException>();
     }
 }
